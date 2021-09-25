394b5e52e170291b4beb4403a1bc093e60eb97e8
Rafael Lima Felix <rafaellfx@gmail.com>
2021-09-24 21:45:02 -0300

    aula 6 time 1:27:33

--- lib/modules/movie_detail/widget/movie_detail_content/movie_detail_content.dart
+++ lib/modules/movie_detail/widget/movie_detail_content/movie_detail_content.dart
@@ -1,5 +1,6 @@
 import 'package:app_filmes/models/movie_detail_model.dart';
 import 'package:app_filmes/modules/movie_detail/widget/movie_detail_content/movie_detail_content_credits.dart';
+import 'package:app_filmes/modules/movie_detail/widget/movie_detail_content/movie_detail_content_main_cast.dart';
 import 'package:app_filmes/modules/movie_detail/widget/movie_detail_content/movie_detail_content_production_companies.dart';
 import 'package:app_filmes/modules/movie_detail/widget/movie_detail_content/movie_detail_content_title.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,8 @@ class MovieDetailContent extends StatelessWidget {
       children: [
         MovieDetailContentTitle(movie: movie),
         MovieDetailContentCredits(movie: movie),
-        MovieDetailContentProductionCompanies(),
+        MovieDetailContentProductionCompanies(movie: movie),
+        MovieDetailContentMainCast(movie: movie),
       ],
     );
   }
